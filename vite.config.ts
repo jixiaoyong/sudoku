7fa3ebff415a1551515d9e6944b172968ec6f02e
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-07-11 22:17:04 +0800

    feat: 修改根目录为sudoku

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+/*
+ * @Author: jixiaoyong
+ * @Email: jixiaoyong1995@gmail.com
+ * @Webiste: https://jixiaoyong.github.io
+ * @Date: 24/07/11
+ * @LastEditors: jixiaoyong
+ * @LastEditTime: 24/07/11
+ * @FilePath: /sudoku/vite.config.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by jixiaoyong, All Rights Reserved. 
+ */
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/sudoku'
 })
