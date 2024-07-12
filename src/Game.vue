0b1f4ccae07d8be049ac442c0a10d4a135b2feba
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-07-12 12:13:11 +0800

    feat: 修改样式，突出当前选中的行列

--- src/Game.vue
+++ src/Game.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import {ref,onUpdated} from 'vue'
-import {getUniqueSudoku} from './sovler/UniqueSudoku'
-import {SudokuContent,SudokuArray,ArraySudoku} from './context/SudokuContext'
-import Footer from './components/layout/Footer.vue';
-import Header from './components/layout/Header.vue';
+import { onUpdated, ref } from 'vue';
+import Action from './components/Action.vue';
 import Difficulty from './components/Difficulty.vue';
 import Timer from './components/Timer.vue';
-import Action from './components/Action.vue';
-import Mode from './components/Mode.vue';
-import GameSection from './components/layout/GameSection.vue';
+import Footer from './components/layout/Footer.vue';
+import Header from './components/layout/Header.vue';
+import { ArraySudoku, SudokuArray, SudokuContent } from './context/SudokuContext';
+import { getUniqueSudoku } from './sovler/UniqueSudoku';
 
 let difficulty = ref("Easy")
 let zero = ref("0")
@@ -16,7 +14,10 @@ let clickIndex = ref<number>(82)
 let isSolved = ref(false)
 let isTimer = ref<boolean>(false)
 
-let numbers:number[] = [1,2,3,4,5,6,7,8,9]
+let currentRow = ref<number>(10);
+let currentColumn = ref<number>(10);
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const temporaryInitArray = ref<string[]>([]);
 const temporarySolvedArray = ref<string[]>([]);
@@ -24,51 +25,53 @@ const temporarySolvedArray = ref<string[]>([]);
 
 const InitArray = ref<SudokuContent[]>(SudokuArray(temporaryInitArray.value))
 
-function newGame() { 
+function newGame() {
   [temporaryInitArray.value, temporarySolvedArray.value] = getUniqueSudoku(difficulty.value, difficulty.value)
   InitArray.value = SudokuArray(temporaryInitArray.value)
   clickIndex.value = 82
   isTimer = ref(true)
 }
 
-onUpdated(()=>{
-  const slovedArray:string[] = ArraySudoku(InitArray.value)
+onUpdated(() => {
+  const slovedArray: string[] = ArraySudoku(InitArray.value)
 
-  var flag:boolean = true;
-  for(var i = 0 ; i < temporarySolvedArray.value.length ; i++){
-    if(temporarySolvedArray.value[i] !== slovedArray[i]){
+  var flag: boolean = true;
+  for (var i = 0; i < temporarySolvedArray.value.length; i++) {
+    if (temporarySolvedArray.value[i] !== slovedArray[i]) {
       flag = false;
     }
   }
-  if(flag === true){
+  if (flag === true) {
     isSolved.value = true
   }
 })
 
-function numberClick(number:number){
-   if(InitArray.value[clickIndex.value].isLocked === true){
+function numberClick(number: number) {
+  if (InitArray.value[clickIndex.value].isLocked === true) {
     InitArray.value[clickIndex.value].content = number.toString()
   }
 }
 
-function erase(){ 
-  if(InitArray.value[clickIndex.value].isLocked === true){
+function erase() {
+  if (InitArray.value[clickIndex.value].isLocked === true) {
     InitArray.value[clickIndex.value].content = ''
   }
 }
 
-function hint(){ 
-  if(InitArray.value[clickIndex.value].isLocked === true){
+function hint() {
+  if (InitArray.value[clickIndex.value].isLocked === true) {
     InitArray.value[clickIndex.value].content = temporarySolvedArray.value[clickIndex.value]
   }
 }
 
-function gameClick(index:number){
+function gameClick(index: number) {
   clickIndex.value = index
+  currentRow.value = Math.floor(index / 9) + 1;
+  currentColumn.value = index % 9 + 1;
 }
 
-function onClickOverlay(){
-  isSolved.value=false
+function onClickOverlay() {
+  isSolved.value = false
   newGame()
 }
 </script>
@@ -82,45 +85,35 @@ function onClickOverlay(){
         <table class="game__board">
           <tbody>
             <tr v-for="row in numbers" :key="row" class="game__row">
-              <template v-for="column in numbers" :key="(column - 1) + (9 * (row - 1))" >
-              <td 
-              class="game__cell" 
-              v-if="InitArray[(column - 1) + (9 * (row - 1))].isLocked !== true"
-              :class="[InitArray[(column - 1) + (9 * (row - 1))].content === zero && InitArray[(column - 1) + (9 * (row - 1))].isLocked === false ? '':'game__cell--filled'
-                       ,clickIndex === (column - 1) + (9 * (row - 1)) ? 'game__cell--highlightselected' : '']"
-              @click="gameClick((column - 1) + (9 * (row - 1)))"
-              >
-                {{ InitArray[(column - 1) + (9 * (row - 1))].content }}
-              </td>
-              <td 
-              class="game__cell" 
-              v-else
-              :class="[InitArray[(column - 1) + (9 * (row - 1))].content !== zero && InitArray[(column - 1) + (9 * (row - 1))].isLocked === true ? 'game__cell--userfilled':''
-                       ,clickIndex === (column - 1) + (9 * (row - 1)) ? 'game__cell--highlightselected' : '']"
-              @click="gameClick((column - 1) + (9 * (row - 1)))"
-              >
-                {{InitArray[(column - 1) + (9 * (row - 1))].content }}
-              </td>
+              <template v-for="column in numbers" :key="(column - 1) + (9 * (row - 1))">
+                <td class="game__cell" :class="[
+                  InitArray[(column - 1) + (9 * (row - 1))].content !== zero && InitArray[(column - 1) + (9 * (row - 1))].isLocked === true ? 'game__cell--userfilled' : '',
+                  clickIndex === (column - 1) + (9 * (row - 1)) ? 'game__cell--highlightselected' : '',
+                  row === currentRow || column === currentColumn ? 'game__cell--highlightrowcolumn' : '',
+                  InitArray[(column - 1) + (9 * (row - 1))].content === zero ? 'game__cell--invisible' : ''
+                ]" @click="gameClick((column - 1) + (9 * (row - 1)))">
+                  {{ InitArray[(column - 1) + (9 * (row - 1))].content }}
+                </td>
               </template>
             </tr>
           </tbody>
         </table>
       </section>
-      
+
       <section class="status">
         <Difficulty v-model="difficulty"></Difficulty>
         <Timer :status="isTimer"></Timer>
 
         <div class="status__numbers">
-          <div v-for="(number,index) in numbers" :key="index" @click="numberClick(number)" class="status__number">
-            {{number}}
+          <div v-for="(number, index) in numbers" :key="index" @click="numberClick(number)" class="status__number">
+            {{ number }}
           </div>
         </div>
 
         <div class="status__actions">
           <!-- <Action action='undo' @action-click="undo()" /> -->
           <Action action='erase' @action-click="erase()" />
-          <Action action='hint' @action-click="hint()"/>
+          <Action action='hint' @action-click="hint()" />
           <!-- <Mode mode='mistakes' @mode-click="mistakes()" /> -->
           <!-- <Mode mode='fast' @mode-click="fast()" /> -->
         </div>
@@ -129,10 +122,28 @@ function onClickOverlay(){
     </div>
     <Footer name="Elone Hoo"></Footer>
 
-    <div @click="onClickOverlay()" :class="[isSolved ? 'overlay overlay--visible':'overlay']">
-        <h2 class="overlay__text">
-          You <span class="overlay__textspan1">solved</span> <span className="overlay__textspan2">it!</span>
-        </h2>
+    <div @click="onClickOverlay()" :class="[isSolved ? 'overlay overlay--visible' : 'overlay']">
+      <h2 class="overlay__text">
+        You <span class="overlay__textspan1">solved</span> <span className="overlay__textspan2">it!</span>
+      </h2>
     </div>
   </div>
 </template>
+
+<style scoped>
+.game__cell {
+  color: black;
+}
+
+.game__cell--highlightselected {
+  color: white;
+}
+
+.game__cell--highlightrowcolumn {
+  background-color: #f0f0f0;
+}
+
+.game__cell--invisible {
+  color: #f0f0f0;
+}
+</style>
